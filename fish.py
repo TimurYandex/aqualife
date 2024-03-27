7f0b2383535de06cde044105afbe10e8f17078cd
TimurYandex <grozovskijtm@yandexlyceum.ru>
2024-03-28 00:53:36 +0300

    Second: rocks, fishes, moving, eating, 3h

--- fish.py
+++ fish.py
@@ -1,3 +1,5 @@
+import random
+
 import pygame
 
 from pygame.locals import K_UP, K_DOWN, K_LEFT, K_RIGHT
@@ -5,6 +7,8 @@ from pygame.locals import K_UP, K_DOWN, K_LEFT, K_RIGHT
 from const import RED, GREEN, DECELERATION, ACCELERATION
 
 all_sprites = pygame.sprite.Group()
+rocks = pygame.sprite.Group()
+fishes = pygame.sprite.Group()
 
 
 class Ball(pygame.sprite.Sprite):
@@ -20,7 +24,9 @@ class Ball(pygame.sprite.Sprite):
 
 
 class Rock(Ball):
-    pass
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+        self.add(rocks)
 
 
 class Fish(Ball):
@@ -30,13 +36,34 @@ class Fish(Ball):
         self.speedy = 1
         self.acceleration = ACCELERATION
         self.deceleration = DECELERATION
+        self.centerp = self.rect.center
+        self.add(fishes)
 
     def additional_check(self):
+        self.centerp = self.rect.center
         ...
+
+    def handle_collisions(self):
+        collided_rocks = pygame.sprite.spritecollide(self, rocks, False,
+                                                     pygame.sprite.collide_mask)
+        if collided_rocks:
+            for rock in collided_rocks:
+                # Вычисляем вектор отскока
+                bounce_vector = pygame.math.Vector2(
+                        self.rect.center) - pygame.math.Vector2(
+                        rock.rect.center)
+                bounce_vector.normalize_ip()
+
+                # Изменяем вектор скорости рыбки
+                self.speedx = bounce_vector.x * abs(
+                        self.speedx) + self.speedx * (0.1 + random.random())
+                self.speedy = bounce_vector.y * abs(
+                        self.speedy) + self.speedy * (0.1 + random.random())
 
     def update(self):
         self.additional_check()
         self.rect = self.rect.move(self.speedx, self.speedy)
+        self.handle_collisions()
 
         # Здесь вы можете добавить логику ограничения рыбки в пределах
         # игрового мира
@@ -49,9 +76,17 @@ class Player(Fish):
         pygame.draw.circle(self.image, self.color, (radius, radius), radius)
 
     def additional_check(self):
+        super().additional_check()
+        # Обработка замедления
+        speed_x = self.speedx ** 2
+        speed_y = self.speedy ** 2
+        if speed_x > 0:
+            self.speedx -= self.deceleration * self.speedx
+        if speed_y > 0:
+            self.speedy -= self.deceleration * self.speedy
+
         keys = pygame.key.get_pressed()
         if keys[K_UP]:
-            print("KEYDOWN in fish")
             self.speedy -= self.acceleration
         if keys[K_DOWN]:
             self.speedy += self.acceleration
@@ -59,16 +94,15 @@ class Player(Fish):
             self.speedx -= self.acceleration
         if keys[K_RIGHT]:
             self.speedx += self.acceleration
-        speed = self.speedx ** 2 + self.speedy ** 2
-        if speed > 0:
-            if self.speedx > 0:
-                self.speedx -= self.deceleration
-            if self.speedx < 0:
-                self.speedx += self.deceleration
-            if self.speedy > 0:
-                self.speedy -= self.deceleration
-            if self.speedy < 0:
-                self.speedy += self.deceleration
+
+    def handle_collisions(self):
+        super().handle_collisions()
+        contacted_fishes = pygame.sprite.spritecollide(self, fishes, False,
+                                                       pygame.sprite.collide_mask)
+        if contacted_fishes:
+            for fish in contacted_fishes:
+                if self is not fish:
+                    fish.kill()
 
 
 class Fry(Fish):
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import pygame
 from pygame.locals import QUIT, KEYDOWN
 
 from const import WIDTH, HEIGHT, FISH_START_SIZE,PINK
-from fish import Player, Fish, all_sprites
+from fish import Player, Fish, Rock, all_sprites
 
 # Создание экрана
 pygame.init()
@@ -12,34 +12,60 @@ pygame.init()
 size = width, height = WIDTH, HEIGHT
 screen = pygame.display.set_mode(size)
 # Установка FPS
-FPS = 50
+FPS = 100
 # Создание игрока
 player = Player(screen.get_rect().centerx, screen.get_rect().centery, FISH_START_SIZE)
 
 
 # Загрузка уровня ??
 
+for i in range(1,20):
+    x = random.randint(0, WIDTH)
+    y = random.randint(0, HEIGHT)
+    radius = random.randint(FISH_START_SIZE/2, FISH_START_SIZE)
+    Fish(x, y, radius)
+
+
+for i in range(1,10):
+    x = random.randint(0, WIDTH//5)*i
+    y = HEIGHT + 1000
+    radius = random.randint(1000, 1100)
+    Rock(x, y, radius)
+
+for i in range(1,10):
+    x = random.randint(0, WIDTH//5)*i
+    y = -1000
+    radius = random.randint(1000, 1100)
+    Rock(x, y, radius)
+
+for i in range(1,10):
+    y = random.randint(0, HEIGHT//5)*i
+    x = WIDTH + 1000
+    radius = random.randint(1000, 1100)
+    Rock(x, y, radius)
+
+for i in range(1,10):
+    y = random.randint(0, HEIGHT//5)*i
+    x = - 1000
+    radius = random.randint(1000, 1100)
+    Rock(x, y, radius)
+
+
+
+clock = pygame.time.Clock()
+running = True
 
 def terminate():
     pygame.quit()
     sys.exit()
 
-
-for _ in range(2):
-    x = random.randint(0, WIDTH)
-    y = random.randint(0, HEIGHT)
-    radius = random.randint(0, FISH_START_SIZE)
-    fish = Fish(x, y, radius)
-
-clock = pygame.time.Clock()
-running = True
 while running:
     # Обработка событий
     for event in pygame.event.get():
         if event.type == QUIT:
             running = False
-        if event.type == KEYDOWN:
-            print("KEYDOWN in main")
+
+
     # Обновление всех спрайтов
     all_sprites.update()
 
